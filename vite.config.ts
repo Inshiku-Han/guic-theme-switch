2fe6dd57ab9573222b392a384ed63572584a73bc
insik-han <insikhan@itez.io>
2022-02-02 14:23:41 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://insik-han.github.io/guic-theme-switch/docs/",
+  base: "https://github.com/insik-han/guic-theme-switch/docs/",
   build: {
     outDir: "docs",
   },
